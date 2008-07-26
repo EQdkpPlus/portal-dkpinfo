e60d1919b72160d35dfb89a78a9b072170ec17e0
osr-corgan <osr-corgan@eqdkp-plus.eu>
2008-07-26 16:02:27 +0000

    Moved remotely

---
+++ language/german.php
@@ -0,0 +1,20 @@
+<?php
+/******************************
+ * EQDKP PLUS
+ * (c) 2008 by EQDKP Plus Dev Team
+ * http://www.eqdkp-plus.com
+ * ------------------
+ * $Id$
+ ******************************/
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+}
+
+$plang = array_merge($plang, array(
+  'dkpinfo'               => 'DKP Infos',
+  'portal_info_raids'     => 'Raids',
+  'portal_info_player'    => 'Spieler',
+  'portal_info_items'     => 'Gegenstände',
+));
+?>
