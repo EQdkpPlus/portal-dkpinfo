10e5b335235c6f921d015690b4e5104237133424
corgan <corgan@eqdkp-plus.eu>
2010-07-09 16:07:05 +0000

    french & spanish translation

--- language/french.php
+++ language/french.php
@@ -1,29 +1,19 @@
 <?php
-/*
- * Project:     EQdkp-Plus
- * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
- * -----------------------------------------------------------------------
- * Began:       2008
- * Date:        $Date: 2008-07-29 00:30:30 +0200 (Di, 29 Jul 2008) $
- * -----------------------------------------------------------------------
- * @author      $Author: osr-corgan $
- * @copyright   2006-2008 Corgan - Stefan Knaak | Wallenium & the EQdkp-Plus Developer Team
- * @link        http://eqdkp-plus.com
- * @package     eqdkp-plus
- * @version     $Rev: 2450 $
- * 
- * $Id: english.php 2450 2008-07-28 22:30:30Z osr-corgan $
- */
 
-if ( !defined('EQDKP_INC') ){
-    header('HTTP/1.0 404 Not Found');exit;
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$plang = array_merge($plang, array(
-  'dkpinfo'               => 'Infos DKP',
-  'portal_info_raids'     => 'Raids',
-  'portal_info_player'    => 'Membres',
-  'portal_info_items'     => 'Objets',
-));
+//Language: French	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 13:55
+//File: module_dkpinfo
+//Source-Language: english
+
+$alang = array( 
+"dkpinfo" => "Infos DKP",
+"portal_info_raids" => "Raids",
+"portal_info_player" => "Membres",
+"portal_info_items" => "Objets",
+ );
+$plang = array_merge($plang, $alang);
 ?>
---
+++ language/spanish.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
+
+//Language: Spanish	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 15:10
+//File: module_dkpinfo
+//Source-Language: english
+
+$alang = array( 
+"dkpinfo" => "Información de DKP",
+"portal_info_raids" => "Bandas",
+"portal_info_player" => "Jugador",
+"portal_info_items" => "Objetos",
+ );
+$plang = array_merge($plang, $alang);
+?>
